f1f5ef928ef756648c199db52330fb0a10d59db4
mochan_tk <takashi1.kawamoto@gmail.com>
2024-08-25 08:47:05 +0000

    commit

--- local/index_chatgpt.js
+++ local/index_chatgpt.js
@@ -4,6 +4,35 @@ const line = require('@line/bot-sdk');
 const express = require('express');
 const { OpenAIClient, AzureKeyCredential } = require("@azure/openai");
 
+// 会話履歴の保存（本当はDBを使った方が良い、ハンズオンなので簡単な方法で実施した）
+let chatHistory = {};  
+
+// データを追加する関数  
+function saveChatHistory(id, currentHistory) {  
+
+  if (!chatHistory[id]) {  
+    chatHistory[id] = [];  
+  }  
+
+  // 履歴は最大10件までにしておく
+  if (chatHistory[id].length >= 10) {  
+    chatHistory[id].splice(0, 2); // 最古のメッセージを削除  
+  }  
+
+  chatHistory[id] = currentHistory;
+}  
+
+// データを取得する関数  
+function getChatHistory(id) {  
+  if (chatHistory[id]) {  
+      console.log(`Customer retrieved: ${id}`);  
+      return chatHistory[id];  
+  } else {  
+      console.log(`Chat History not found for id: ${id}`);  
+      return []; 
+  }  
+}  
+
 // create LINE SDK config from env variables
 const config = {
   channelSecret: process.env.CHANNEL_SECRET,
@@ -32,6 +61,8 @@ app.post('/callback', line.middleware(config), (req, res) => {
 
 // event handler
 async function handleEvent(event) {
+  const userId = event.source.userId;
+
   if (event.type !== 'message' || event.message.type !== 'text') {
     // ignore non-text-message event
     return Promise.resolve(null);
@@ -45,14 +76,22 @@ async function handleEvent(event) {
 
   const deploymentId = process.env.AZURE_OPENAI_DEPLOYMENT_NAME; // model = "deployment name".
 
-  const messages = [
-    { role: "user", content: event.message.text },
+  // モデルに渡すメッセージ情報の作成
+  let messages = [
+    { role: "system", content: "あなたは日本語を話すAIアシスタントです。" },
   ];
+
+  let lastHistory = getChatHistory(userId);
+
+  messages = messages.concat(lastHistory)
+
+  // ユーザーのメッセージをセット
+  messages.push({ role: "user", content: event.message.text });
 
   console.log(`Messages: ${messages.map((m) => m.content).join("\n")}`);
 
   let msg = '';
-  const events = await openaiClient.streamChatCompletions(deploymentId, messages, { maxTokens: 128 });
+  const events = await openaiClient.streamChatCompletions(deploymentId, messages, { maxTokens: 4096 });
   for await (const event of events) {
     for (const choice of event.choices) {
       const delta = choice.delta?.content;
@@ -62,6 +101,16 @@ async function handleEvent(event) {
       }
     }
   }
+
+  // 会話履歴の保存
+  lastHistory.push({ role: "user", content: event.message.text });
+  lastHistory.push({ role: "assistant", content: msg });
+  saveChatHistory(userId, lastHistory);
+
+  // // DEBUG用:for文でメッセージを順番に処理  
+  // for (let i = 0; i < lastHistory.length; i++) {  
+  //   console.log(`Message ${i + 1}:　Content - ${lastHistory[i].content}`);  
+  // }  
 
   // create an echoing text message
   console.log(msg);
